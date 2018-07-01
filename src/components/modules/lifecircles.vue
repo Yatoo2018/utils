7454dfe3e746518be2af55c7ca61eef7ca2f957c
Yatoo2018 <tanpf2018@163.com>
2018-07-01 14:16:25 +0800

    add vue-lifecircles的组件

---
+++ src/components/modules/lifecircles.vue
@@ -0,0 +1,30 @@
+/**
+ * 文件名称: lifecircles
+ * 创建时间: 2018/06/29 11:17
+ * 创建用户: Yatoo2018
+ * 用户邮箱: tanpf2018@163.com
+ * 所属项目: zhenchuanx
+ * 项目路径：
+ *
+ * 修改原因: xxx
+ * 修改时间: xxx
+ * 修改人名: xxx
+ */
+<template>
+
+</template>
+<script>
+  console.log()
+export default {
+  name: 'lifecircles',
+  props: ['parentData'],
+  data() {
+    return {
+      data: "我是data中的"
+    }
+  }
+}
+</script>
+<style>
+
+</style>
